67ed40451211ca0984ed3789ba89d313707b6b9b
Eduardo Girón <eduardogiron@gmail.com>
2010-07-11 19:20:27 -0500

    Clase de Audio Completa

--- mx/cinvestav/movil/media/Audio.java
+++ mx/cinvestav/movil/media/Audio.java
@@ -4,11 +4,45 @@
  */
 
 package mx.cinvestav.movil.media;
-
+import javax.microedition.media.*;
+import java.io.*;
 /**
  *
  * @author eduardogiron
  */
 public class Audio {
+    Player player;
+    
+    public Audio(){
+        //InputStream in = getClass().getResourceAsStream("/relax.wav");
+        try{
+            InputStream in = getClass().getResourceAsStream("/DGrayM.mp3");
+            player = Manager.createPlayer(in,"audio/mpeg");
+            player.prefetch();
+            player.setLoopCount(-1);
+            
 
+        }catch(IOException ioe){
+            System.out.println(ioe);
+        }catch(MediaException me){
+
+        }
+                
+    }
+    public void start_song(){
+        try {
+            player.start();
+        } catch (MediaException me) {
+            System.out.println(me);
+        }
+
+    }
+    public void stop_song(){
+        try {
+            player.stop();
+        } catch (MediaException me) {
+            System.out.println(me);
+        }
+
+    }
 }
