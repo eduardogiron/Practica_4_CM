828e264bf85cfed0645bdb8ecee62c2d141cea7a
Eduardo Girón <eduardogiron@gmail.com>
2010-07-11 19:20:56 -0500

    Clase de Video Completa

--- mx/cinvestav/movil/media/Video.java
+++ mx/cinvestav/movil/media/Video.java
@@ -5,10 +5,53 @@
 
 package mx.cinvestav.movil.media;
 
+import javax.microedition.media.*;
+import javax.microedition.media.control.*;
+import javax.microedition.lcdui.*; 
+import java.io.*;
+
 /**
  *
  * @author eduardogiron
  */
 public class Video {
+    private Player mPlayer = null;
+    private VideoControl mVidc;
+    private Item mVideoItem;
+
+    public  Video(){
+        try{
+            InputStream in = getClass().getResourceAsStream("/video.mpg");
+            mPlayer = Manager.createPlayer(in, "video/mpeg");
+            mPlayer.realize();
+            if ((mVidc = (VideoControl)mPlayer.getControl("VideoControl")) != null) {
+                mVideoItem = (Item)mVidc.initDisplayMode(VideoControl.USE_GUI_PRIMITIVE, null); 
+                }
+        }catch(IOException ioe){
+            System.out.println(ioe);
+        }catch(MediaException me){
+
+        }
+        
+    }
+    public Item pantalla_video(){
+        return mVideoItem;
+    }
+    public void start_video(){
+        try {
+            mPlayer.start();
+        } catch (MediaException me) {
+            System.out.println(me);
+        }
+
+    }
+    public void stop_video(){
+        try {
+            mPlayer.stop();
+        } catch (MediaException me) {
+            System.out.println(me);
+        }
+
+    }
 
 }
